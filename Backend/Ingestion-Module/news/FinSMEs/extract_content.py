20ca8fc1b43279392dc28955cab2de61c2dea7db
adolfiscariot <m10mathenge@gmail.com>
2025-07-23 14:09:09 +0300

    feat: Fetching done. Next is extraction with Gemini

--- Backend/Ingestion-Module/news/FinSMEs/extract_content.py
+++ Backend/Ingestion-Module/news/FinSMEs/extract_content.py
@@ -1,3 +1,13 @@
-from news.FinSMEs.fetch import fetch_ai_funding_article_links
+import os
 import httpx
 from lxml import etree
+from dotenv import load_dotenv
+import google.generativeai as genai
+
+#Import env variables
+load_dotenv(verbose=True, override=True)
+
+#Gemini API Key
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+genai.configure(api_key=GOOGLE_API_KEY)
+
