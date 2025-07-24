29baba884bc331c33429f72ccac893594da7b53f
adolfiscariot <m10mathenge@gmail.com>
2025-07-24 19:58:13 +0300

    feat: Done w/paragraph extraction from FinSMEs

--- Backend/Ingestion-Module/ai-extraction/extract_content.py
+++ Backend/Ingestion-Module/ai-extraction/extract_content.py
@@ -10,4 +10,3 @@ load_dotenv(verbose=True, override=True)
 #Gemini API Key
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 genai.configure(api_key=GOOGLE_API_KEY)
-
--- Backend/Ingestion-Module/news/FinSMEs/fetch.py
+++ Backend/Ingestion-Module/news/FinSMEs/fetch.py
@@ -4,15 +4,13 @@ import time
 import httpx
 import asyncio
 import logging
-from lxml import etree
-from utils.data_structures.news_data_structure import fetched_data
+from lxml import etree, html
+from typing import Dict, List
 
 #====================FIND URL====================
 """
 This website's sitemap is a list of numbered sitemaps.
 We therefore have to parse them until we find the latest one.
-Once we find it, we store it and the next search starts from
-that one.
 """
 #===============PARENT SITEMAP================
 URL = "https://www.finsmes.com/wp-sitemap.xml"
@@ -28,7 +26,7 @@ Since the sitemaps are organized from 1 to the latest, we only
 need the latest one. The method below finds the latest sidemap.
 """
 async def find_newest_sitemap(url: str)->str:
-    print("Finding latest sitemap")
+    logging.info("Fetching latest sitemap...")
 
     #Regex to search for posts only
     pattern = re.compile(r'-post-(\d+)\.xml')
@@ -57,6 +55,7 @@ async def find_newest_sitemap(url: str)->str:
                         latest_sitemap = url
 
             print(f"Latest sitemap: {latest_sitemap}")
+            logging.info("Fetching latest sitemap done")
             return latest_sitemap
 
         except Exception as e:
@@ -64,15 +63,13 @@ async def find_newest_sitemap(url: str)->str:
             return
 
 #Extract all ai funding article links from the latest sitemap
-async def fetch_ai_funding_article_links(url: str)->dict:
-    logging.info(f"The URL is {url}")
+async def fetch_ai_funding_article_links(url: str)->list:
+    logging.info(f"Fetching AI specific urls...")
     try:
         #=======FETCH DATA==========
         async with httpx.AsyncClient() as client:
             response = await client.get(url)
-            if(response.status_code != 200):
-                logging.error("Couldn't fetch data")
-                raise Exception(f"Couldn't fetch data. {response.status_code}")
+            response.raise_for_status()
 
         root = etree.fromstring(response.content) #type: ignore         
 
@@ -85,21 +82,80 @@ async def fetch_ai_funding_article_links(url: str)->dict:
                 ai_funding_articles.append(article_link)
 
         print(json.dumps(ai_funding_articles, indent=2))
-        return [set(ai_funding_articles)]
+        logging.info("Feching AI specific urls done")
+        return list(set(ai_funding_articles))
 
     except Exception as e:
         logging.error(f"Error fetching/parsing {URL}: {str(e)}")
 
+"""
+Now we open the links and extract the necessary paragraphs before feeding it to the LLM
+"""
+async def get_paragraphs(urls: list) -> Dict[str, List[str]]:
+    logging.info("Getting paragraphs from urls...")
+    if not urls:
+        logging.error("List of AI specific Urls Not Found")
+        return {"urls": [], "paragraphs": []}
+    
+    try:
+        results = {"urls": [], "paragraphs": []}
+        async with httpx.AsyncClient(timeout=30.0, follow_redirects=True) as client:
+            tasks = [extract_paragraphs(client, url) for url in urls]                
+            
+            """
+            Coroutine below is an awaitable and not the actual coroutine
+            which is why we have to await it
+            """
+
+            for coroutine in asyncio.as_completed(tasks):
+                url, paragraphs = await coroutine 
+                results["urls"].append(url)
+                results["paragraphs"].append(paragraphs)
+
+        logging.info("Done getting paragraphs from urls")
+        return results
+
+    except Exception as e:
+        logging.error("Failed getting paragraphs from urls")
+    
+    return {"": [""]}
+            
+#Function that does the actual extraction
+async def extract_paragraphs(client: httpx.AsyncClient, url: str)->tuple[str, list[str]]:
+    logging.info(f"Fetching paragraphs from {url}...")
+    try:
+        response = await client.get(url)
+        response.raise_for_status()
+
+        root = html.fromstring(response.text)
+
+        #Extract paragraphs from the class below
+        paragraph_nodes = root.xpath("//div[contains(@class, 'tdb-block-inner') and contains(@class, 'td-fix-index')]//p")
+        paragraphs = [node.text_content().strip() for node in paragraph_nodes if node.text_content().strip()]
+
+        logging.info(f"Done fetching paragraphs")
+        return url, paragraphs
+
+    except httpx.HTTPStatusError as e:
+        logging.error(f"Failed to fetch paragraphs from {url}")
+    except Exception as e:
+        logging.error(f"Error processing {url}: {str(e)}")
+        
+    return url, []
+
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
     async def main(): #Allows us to run the code asynchronously to avoid blocking
+        logging.info("Fetching from FinSMEs...")
         current_time = time.perf_counter()
         newest_sitemap = await find_newest_sitemap(URL)
-        await fetch_ai_funding_article_links(newest_sitemap)
+        if newest_sitemap:
+            ai_urls = await fetch_ai_funding_article_links(newest_sitemap)
+            if ai_urls:
+                results = await get_paragraphs(ai_urls)
+                print(json.dumps(results, indent=2))
+        logging.info("Done fetching from FinSMEs.Time for AI information extraction")
         running_time = time.perf_counter() - current_time
         print(f"Program ran for {running_time}")
     
     asyncio.run(main())
-
-
-
-
