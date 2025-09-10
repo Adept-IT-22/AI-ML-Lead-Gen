e0b9e0bf4537ae187932795d3246d9a480ff13d6
adolfiscariot <m10mathenge@gmail.com>
2025-09-10 13:10:22 +0300

    fix: style the navbar so components fit

--- Frontend/lead-gen/src/app/app.routes.ts
+++ Frontend/lead-gen/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './Pages/home/home.component';
 import { AnalyticsComponent } from './Pages/analytics/analytics.component';
 import { LeadsTableComponent } from './@shared/Components/leads/leads.component'; 
-import { LeadsPageComponent } from './@shared/Components/leads-page/leads-page.component';
+import { LeadsPageComponent } from './Pages/leads-page/leads-page.component';
 
 export const routes: Routes = [
     {
