65df7202700f326314d23e579b0e563b82dfc1f9
adolfiscariot <m10mathenge@gmail.com>
2025-09-24 09:52:42 +0300

    fix: filter by score can now filter more values

--- Backend/utils/locations.py
+++ Backend/utils/locations.py
@@ -4,49 +4,16 @@ locations ={
     #North American Countries
     "north american countries": { "united states", "canada"},
 
-    #U.S. Cities
-    "us cities": {
-    "new york city", "los angeles", "chicago", "houston", "phoenix",
-    "philadelphia", "san antonio", "san diego", "dallas", "san jose",
-    "austin", "jacksonville", "fort worth", "columbus", "charlotte",
-    "san francisco", "indianapolis", "seattle", "denver",
-    "washington, d.c.", "boston", "el paso", "nashville", "detroit",
-    "oklahoma city", "portland", "las vegas", "memphis", "louisville",
-    "baltimore", "milwaukee", "albuquerque", "tucson", "fresno",
-    "sacramento", "kansas city", "mesa", "atlanta", "omaha",
-    "colorado springs", "raleigh", "miami", "long beach",
-    "virginia beach", "oakland", "minneapolis", "tulsa", "tampa",
-    "arlington", "new orleans"},
-
-    #African Countries (Only Kenya & S.Africa)
-    "african countries": {
-    "kenya", "south africa"
-    },
-    #African Cities
-    "African Cities": {
-    "johannesburg", "nairobi",
-    "cape town", "pretoria"},
-    
     #European Countries
     "european countries": {
     "austria", "belgium", "bulgaria", "croatia", "cyprus",
     "czech republic", "denmark", "estonia", "finland", "france",
-    "germany", "greece", "hungary", "ireland", "italy", "latvia",
+    "germany", "greece", "hungary", "ireland", "latvia",
     "lithuania", "luxembourg", "malta", "netherlands", "poland",
     "portugal", "romania", "slovakia", "slovenia", "spain", "sweden",
     "albania", "andorra", "belarus", "bosnia and herzegovina",
     "iceland", "kosovo", "liechtenstein", "moldova", "monaco",
-    "montenegro", "north macedonia", "norway", "san marino", "serbia",
+    "montenegro", "north macedonia", "norway", "serbia",
     "switzerland", "ukraine", "united kingdom", "vatican city"},
 
-    #European Cities
-    "european cities": {
-    "london", "paris", "berlin", "madrid", "rome",
-    "vienna", "amsterdam", "brussels", "stockholm", "oslo",
-    "copenhagen", "helsinki", "lisbon", "prague", "budapest",
-    "warsaw", "athens", "dublin", "zurich", "barcelona",
-    "munich", "milan", "hamburg", "frankfurt", "bucharest",
-    "sofia", "zagreb", "belgrade", "riga", "vilnius",
-    "tallinn", "luxembourg", "ljubljana", "sarajevo", "bratislava",
-    "reykjavik", "valletta", "podgorica", "skopje", "tirana"},
 }
