28715393138ab01f65e3b1673d4ac3dc9094c3b3
adolfiscariot <m10mathenge@gmail.com>
2026-01-27 11:46:15 +0300

    feat: added unsubscribe button to each email

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './Pages/home/home.component';
 import { AnalyticsComponent } from './Pages/analytics/analytics.component';
-import { LeadsTableComponent } from './@shared/Components/leads/leads.component'; 
+import { LeadsTableComponent } from './@shared/Components/leads/leads.component';
 import { LeadsPageComponent } from './Pages/leads-page/leads-page.component';
-import { EventsComponent} from './Pages/events/events.component';
+import { EventsComponent } from './Pages/events/events.component';
 import { EngagementComponent } from './Pages/engagement/engagement.component';
 import { SettingsComponent } from './@shared/Components/settings/settings.component';
 
@@ -11,18 +11,20 @@ export const routes: Routes = [
     {
         path: '',
         component: HomeComponent
-    },    
+    },
     {
         path: 'analytics',
         component: AnalyticsComponent
     },
 
-    {   path: 'company/:id',
+    {
+        path: 'company/:id',
         loadComponent: () =>
             import('./@shared/Components/company-details/company-details.component').then(m => m.CompanyDetailsComponent),
     },
 
-    {   path: 'emails/:company_id',
+    {
+        path: 'emails/:company_id',
         loadComponent: () =>
             import('./Pages/emails/emails.component').then(m => m.EmailsComponent)
     },
@@ -40,10 +42,16 @@ export const routes: Routes = [
     {
         path: 'engagement',
         component: EngagementComponent
-    },  
+    },
 
     {
         path: 'settings',
         component: SettingsComponent
+    },
+
+    {
+        path: 'unsubscribe',
+        loadComponent: () =>
+            import('./Pages/unsubscribe/unsubscribe.component').then(m => m.UnsubscribeComponent)
     },
 ];
